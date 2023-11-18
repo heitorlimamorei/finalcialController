0292be25a38b94f6b90815b4f4f145ea59ec432e
heitorlimamorei <heitorlmoreira2017@outlook.com>
2023-11-18 00:50:48 +0000

    feat: add of objectives page

---
+++ src/pages/objectives/sheet/[sheetId]/index.tsx
@@ -0,0 +1,11 @@
+import Layout from '../../../../components/template/Layout';
+
+const Objectives = () => {
+  return (
+    <Layout titulo="Pagina inicial" subtitulo="Estamos construindo um admin template">
+      
+    </Layout>
+  );
+};
+
+export default Objectives;
