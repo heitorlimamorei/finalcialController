a01df947a7f3345465b26cb71b7fe146ec5dcafc
felipeRese <feliperese2018@gmail.com>
2024-07-10 14:42:55 -0300

    fix:lint

--- src/app/dashboard/[id]/page.tsx
+++ src/app/dashboard/[id]/page.tsx
@@ -30,8 +30,18 @@ export default function Dashboard() {
   return (
     <div className="flex flex-col h-screen w-screen justify-between bg-gray-100 text-black overflow-y-scroll">
       {selectedButton === 'home' && width < 768 ? <DashboardMobilePage name="Felipe Rese" /> : null}
+      {selectedButton === 'config' ? (
+        <div className="h-full w-full">
+          <div className="flex flex-row h-[20%] w-full items-center justify-center">
+            <Image src={'/icon-black-no-bg.png'} alt="Profile picture" width={100} height={100} />
+            <div className="h-full w-fit flex flex-col justify-center px-5">
+              <h1 className="text-xl font-bold">Felipe Rese</h1>
+              <p>feliperese2018@gmail.com</p>
+            </div>
+          </div>
+        </div>
+      ) : null}
       <NavBar selectedButton={selectedButton} handleSelectButton={handleSelectButton} />
     </div>
   );
 }
-
