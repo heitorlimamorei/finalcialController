7b11b8335fededb6e37945f81994235d461890bc
felipeRese <feliperese2018@gmail.com>
2024-07-16 18:34:50 -0300

    fix:lint

--- src/components/icons/Icons.tsx
+++ src/components/icons/Icons.tsx
@@ -92,3 +92,19 @@ export const TransferIcon = (color: string = '#000000', size: number) => (
     />
   </svg>
 );
+
+export const MinusIcon = (color: string = '#000000', size: number) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke={color}
+    className={`size-${size}`}>
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M15 12H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+    />
+  </svg>
+);
