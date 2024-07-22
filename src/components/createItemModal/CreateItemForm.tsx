a5fbed0758c9e8bab9c7a02804ed471ad06ff57a
felipeRese <feliperese2018@gmail.com>
2024-07-21 21:59:01 -0300

    feat: added category select

--- src/components/createItemModal/CreateItemForm.tsx
+++ src/components/createItemModal/CreateItemForm.tsx
@@ -1,6 +1,10 @@
 import { type ReactElement } from 'react';
 import { useState } from 'react';
 
+import { useCategory } from '@/hook/useCategory';
+import { ICategory } from '@/types/category';
+import { IUser } from '@/types/user';
+import axios from 'axios';
 import Button from '../common/Button';
 import CurrencyInput from '../common/CurrencyInput';
 import DateInput from '../common/DateInput';
@@ -10,20 +14,53 @@ import { ItemType } from './CreateItemModal';
 interface CreateItemFormProps {
   type: ItemType | undefined;
   selectedType: ItemType | undefined;
+  user: IUser;
+  accountId: string;
 }
 
 export default function CreateItemForm({ selectedType, type }: CreateItemFormProps): ReactElement {
+const api = 'https://financial-controller-backend.onrender.com/api/v1';
+export default function CreateItemForm({
+  selectedType,
+  type,
+  user,
+  accountId,
+}: CreateItemFormProps): ReactElement {
   const today: Date = new Date();
+  const { getCategories } = useCategory();
   const [amount, setAmount] = useState<number>(0);
   const [name, setName] = useState<string>('');
   const [description, setDescription] = useState<string>('');
   const [date, setDate] = useState<Date>(today);
+  const [selectedCategory, setSelectedCategory] = useState<string>('');
+  const [selectedSubcategory, setSelectedSubcategory] = useState<string>('');
+  // All categories list
+  const [categories, setCategories] = useState<ICategory[]>([]);
 
-  function handleChangeName(e: any) {
+  useEffect(() => {
+    const fetchCategories = async (sheetId: string) => {
+      try {
+        const categories: ICategory[] = await getCategories(sheetId);
+        setCategories(categories);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchCategories(user.personalSheetId);
+  }, [getCategories, user.personalSheetId]);
+  function handleChangeName(e: ChangeEvent<HTMLInputElement>) {
     setName(e.target.value);
   }
-  function handleChangeDescription(e: any) {
+  function handleChangeDescription(e: ChangeEvent<HTMLInputElement>) {
     setDescription(e.target.value);
+  }
+  function handleChangeCategory(e: ChangeEvent<HTMLSelectElement>) {
+    setSelectedCategory(e.target.value);
+    setSelectedSubcategory(''); // Reset subcategory when category changes
+  }
+
+  function handleChangeSubcategory(e: ChangeEvent<HTMLSelectElement>) {
+    setSelectedSubcategory(e.target.value);
   }
   return (
     <div
@@ -61,9 +98,42 @@ export default function CreateItemForm({ selectedType, type }: CreateItemFormPro
         </div>
 
         <div className="w-full my-2 flex flex-col">
-          <label>Categoria</label>
           <select name="" id="" className="p-2 rounded-xl bg-gray-200"></select>
+          <label htmlFor="category">Categoria</label>
+          <select
+            id="category"
+            className="p-2 rounded-xl bg-gray-200"
+            value={selectedCategory}
+            onChange={handleChangeCategory}>
+            <option value="">Selecione</option>
+            {categories
+              .filter((category) => category.mainCategoryId === undefined)
+              .map((category) => (
+                <option key={category.id} value={category.id}>
+                  {category.name}
+                </option>
+              ))}
+          </select>
         </div>
+        {categories.some((category) => category.mainCategoryId === selectedCategory) && (
+          <div className="w-full my-2 flex flex-col">
+            <label htmlFor="subcategory">Subcategoria</label>
+            <select
+              id="subcategory"
+              className="p-2 rounded-xl bg-gray-200"
+              value={selectedSubcategory}
+              onChange={handleChangeSubcategory}>
+              <option value="">Selecione</option>
+              {categories
+                .filter((category) => category.mainCategoryId === selectedCategory)
+                .map((subcategory) => (
+                  <option key={subcategory.id} value={subcategory.id}>
+                    {subcategory.name}
+                  </option>
+                ))}
+            </select>
+          </div>
+        )}
 
         <Button
           className={`w-full m-2 ${type == 'income' ? 'bg-green-500' : 'bg-red-500'} text-xl font-bold text-white`}>
---
+++ src/hook/useCategory.ts
@@ -0,0 +1,48 @@
+import axios from 'axios';
+
+const api = 'https://financial-controller-backend.onrender.com/api/v1';
+
+export function useCategory() {
+  async function getCategories(sheetId: string) {
+    const categories = await axios.get(`${api}/category?sheetId=${sheetId}`);
+    return categories.data;
+  }
+
+  async function getCategory(sheetId: string, categoryId: string) {
+    const category = await axios.get(
+      `${api}/category/${categoryId}?sheetId=${sheetId}`,
+    );
+    return category.data;
+  }
+
+  async function createCategory(
+    sheetId: string,
+    name: string,
+    ownerId: string,
+    type: string,
+    imagePath: string,
+  ) {
+    const response = await axios.post(`${api}/category`, {
+      sheetId,
+      name,
+      ownerId,
+      type,
+      imagePath,
+    });
+    return response.data;
+  }
+
+  async function deleteCategory(sheetId: string, categoryId: string) {
+    const category = await axios.delete(
+      `${api}/category/${categoryId}?sheetId=${sheetId}`,
+    );
+    return;
+  }
+
+  return {
+    getCategories,
+    getCategory,
+    createCategory,
+    deleteCategory,
+  };
+}
