ceeeda3392dbf3a416acf8fd323fc61c3fa9c9b7
Heitor Lima Moreira <61924603+heitorlimamorei@users.noreply.github.com>
2024-07-21 23:51:06 -0300

    fix: changed the date type in Item to firebaTimesStamp and create the INewItem interface

--- src/types/item.ts
+++ src/types/item.ts
@@ -1,5 +1,19 @@
+import { firebaseTimesStampType } from './utils.type';
+
 export interface IItem {
   id: string;
+  sheetId: string;
+  categoryId: string;
+  ownerId: string;
+  name: string;
+  description: string;
+  accountId: string;
+  amount: number;
+  date: firebaseTimesStampType;
+  type: string;
+}
+
+export interface INewItem {
   sheetId: string;
   categoryId: string;
   ownerId: string;
