d5e6d9bbc93a139a519e4c08dab8a5ebe534385d
felipeRese <feliperese2018@gmail.com>
2024-07-24 17:34:26 -0300

    feat/tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,9 @@ const config: Config = {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/tailwind-datepicker-react/dist/**/*.js',
   ],
+  darkMode: 'class',
   theme: {
     extend: {
       backgroundImage: {
