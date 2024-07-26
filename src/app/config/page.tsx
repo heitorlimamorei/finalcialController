795b2e7b6f78cc41d832dd3e4afa7ece0843ef44
felipeRese <feliperese2018@gmail.com>
2024-07-26 15:02:26 -0300

    feat:added support for ManageAccountsModal

--- src/app/config/page.tsx
+++ src/app/config/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { useState } from 'react';
+
 import { IUser } from '@/types/user';
 import fetcher from '@/utils/fetcher';
 import useSWR from 'swr';
@@ -7,6 +9,8 @@ import useSWR from 'swr';
 import Loading from '@/components/common/Loading';
 import NavBar from '@/components/common/NavBar';
 import ThemeSwitcher from '@/components/common/ThemeSwitcher';
+import { ChevronRightIcon } from '@/components/icons/Icons';
+import ManageAccountsModal from '@/components/manageAccountsModal/ManageAccountsModal';
 import { ConfigProfile } from '@/components/mobile/configMobile/ConfigProfile';
 
 interface IConfigProps {
@@ -17,17 +21,30 @@ interface IConfigProps {
 
 export default function Config(props: IConfigProps) {
   const { data: user, isLoading } = useSWR<IUser>(`/user/${props.searchParams.u}`, fetcher);
+  const [isAccountsOpen, setIsAccountsOpen] = useState(false);
   if (isLoading || !user) {
     return <Loading />;
   }
   return (
     <div className="flex flex-col h-screen w-screen justify-between dark:bg-zinc-800 dark:text-white bg-gray-100 text-black overflow-y-scroll">
+      <ManageAccountsModal
+        isOpen={isAccountsOpen}
+        onClose={() => setIsAccountsOpen(false)}
+        ownerId={props.searchParams.u}
+      />
       <div className="flex flex-col h-screen w-screen justify-between dark:bg-zinc-800 dark:text-white bg-gray-100 text-black overflow-y-scroll">
         <ConfigProfile user={user} />
         <div className="w-full h-full">
           <div className="border-t-2 border-y-gray-300 py-5 px-3 flex flex-row items-center justify-between">
             <h1 className=" text-2xl font-semibold ">Tema</h1>
             <ThemeSwitcher />
+          </div>
+
+          <div
+            className="border-t-2 border-y-gray-300 py-5 px-3 flex flex-row items-center justify-between"
+            onClick={() => setIsAccountsOpen(true)}>
+            <h1 className=" text-2xl font-semibold ">Contas</h1>
+            {ChevronRightIcon(6)}
           </div>
         </div>
       </div>
