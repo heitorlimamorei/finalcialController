ca036b21a9907e5cf669aec4a3875f0c5e23478c
felipeRese <feliperese2018@gmail.com>
2024-07-26 15:10:09 -0300

    fix: lint

--- src/schemas/createAccountSchema.ts
+++ src/schemas/createAccountSchema.ts
@@ -2,12 +2,6 @@ import { z } from 'zod';
 
 export const createAccountSchema = z.object({
   name: z.string().min(1),
-  financial_institution: z
-    .string()
-    .min(1, { message: 'Instituição Financeira é obrigatório' }),
-  balance: z
-    .number()
-    .nonnegative({ message: 'Saldo não pode ser negativo' })
-    .default(0)
-    .optional(),
+  financial_institution: z.string().min(1, { message: 'Instituição Financeira é obrigatório' }),
+  balance: z.number().nonnegative({ message: 'Saldo não pode ser negativo' }).default(0).optional(),
 });
