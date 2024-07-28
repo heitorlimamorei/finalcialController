555a3a8f63fea40fa3324093da59066139661162
Heitor Lima Moreira <61924603+heitorlimamorei@users.noreply.github.com>
2024-07-28 00:41:59 -0300

    feat: removing selectedaccount state, add save account choice in the url query params, add change selectedaccount through a router push with the account id

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
+
 import { IAccount } from '@/types/account';
 import { IUser } from '@/types/user';
 import fetcher from '@/utils/fetcher';
@@ -13,11 +15,15 @@ import WelcomeHeader from '@/components/mobile/dashboardMobile/WelcomeHeader';
 interface IDashboardProps {
   searchParams: {
     u: string;
+    account?: string;
   };
 }
 
 export default function Dashboard(props: IDashboardProps) {
   const id = props.searchParams.u;
+  const accountId = props.searchParams?.account;
+
+  const router = useRouter();
 
   const { data: accounts, error: accountsError } = useSWR<IAccount[]>(
     `/account?owid=${id}`,
@@ -34,11 +40,19 @@ export default function Dashboard(props: IDashboardProps) {
     return <Loading />;
   }
 
+  if (!accountId) router.push(`/dashboard?u=${id}&account=${accounts[0].id}`);
+
+  const account = accounts.find((c) => c.id === accountId);
+
+  if (!account) {
+    return <div>Error loading account data...</div>;
+  }
+
   return (
     <div className="flex flex-col h-screen w-screen justify-between bg-gray-100 text-black dark:bg-zinc-800 dark:text-white overflow-y-scroll">
       <div className="w-full h-full overflow-y-hidden">
         <WelcomeHeader name={user.name} />
-        <DashboardMobile user={user} accounts={accounts} />
+        <DashboardMobile user={user} accounts={accounts} account={account} />
       </div>
       <NavBar user={user} selectedButton={'home'} />
     </div>
