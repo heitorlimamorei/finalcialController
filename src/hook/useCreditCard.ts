5e3de3ebd814466606fb149c83340f772ae8fe26
Heitor Lima Moreira <61924603+heitorlimamorei@users.noreply.github.com>
2024-08-28 17:19:34 -0300

    feat: add of useCreditCard hook, do changes in creditCard types that cointains ICreditCard interface, IAPICreeditCard that stads for creditCard as api resp format, and InewCreditCard

---
+++ src/hook/useCreditCard.ts
@@ -0,0 +1,51 @@
+import { IAPICreditCard, ICreditCard, INewCreditCard } from '@/types/creditCard';
+import { firestoreTimestampToDate } from '@/utils/datefunctions';
+import fetcher from '@/utils/fetcher';
+import axios from 'axios';
+import useSWR, { mutate } from 'swr';
+
+const api = process.env.NEXT_PUBLIC_API_URL;
+
+export default function useCreditCard(owid: string) {
+  const { data: creditCardRaw, isLoading } = useSWR<IAPICreditCard[]>(
+    `/credit-card?owid=${owid}`,
+    fetcher,
+  );
+
+  const sanitizeCreditCard = (c: IAPICreditCard): ICreditCard => {
+    return {
+      ...c,
+      expirationDate: firestoreTimestampToDate(c.expirationDate),
+      lastBill: c?.lastBill ? firestoreTimestampToDate(c.lastBill) : null,
+    };
+  };
+
+  async function handleDeleteCard(id: string) {
+    try {
+      const response = await axios.delete(`${api}/credit-card/${id}?owid=${owid}`);
+      mutate('/credit-card?owid=' + owid);
+      console.log(response);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  async function handleCreateCreditCard(newCreditCard: INewCreditCard) {
+    try {
+      const response = await axios.post(`${api}/credit-card`, newCreditCard);
+      mutate('/credit-card?owid=' + owid);
+      console.log(response);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  const creditCards = creditCardRaw?.map(sanitizeCreditCard);
+
+  return {
+    creditCards,
+    isLoading,
+    handleDeleteCard,
+    handleCreateCreditCard,
+  };
+}
--- src/types/creditCard.ts
+++ src/types/creditCard.ts
@@ -1,9 +1,39 @@
+import { firebaseTimesStampType } from './utils.type';
+
+export type CardBrands = 'visa' | 'mastercard' | 'elo';
+
 export interface ICreditCard {
-  nickname: string;
+  id: string;
   ownerId: string;
-  flag: string;
-  spendingLimit: number;
-  expires: Date;
-  lastNumbers: string;
+  nickname: string;
+  cardNumber: string;
+  flag: CardBrands;
+  expirationDate: Date;
+  lastBill?: Date | null;
   financialInstitution: string;
+  spendingLimit: number;
+  availableLimit: number;
+}
+
+export interface IAPICreditCard {
+  id: string;
+  ownerId: string;
+  nickname: string;
+  cardNumber: string;
+  flag: CardBrands;
+  expirationDate: firebaseTimesStampType;
+  lastBill?: firebaseTimesStampType;
+  financialInstitution: string;
+  spendingLimit: number;
+  availableLimit: number;
+}
+
+export interface INewCreditCard {
+  ownerId: string;
+  nickname: string;
+  cardNumber: string;
+  flag: CardBrands;
+  expirationDate: string;
+  financialInstitution: string;
+  spendingLimit: number;
 }
