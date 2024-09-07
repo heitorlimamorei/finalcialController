d43a2692cc86ddff44ec5cb1b47b5760e34123ff
felipeRese <feliperese2018@gmail.com>
2024-09-07 14:14:31 -0300

    feat: added style for credit card balance card

--- src/components/common/CreditCard.tsx
+++ src/components/common/CreditCard.tsx
@@ -12,45 +12,63 @@ import Button from './Button';
 
 interface ICreditCardProps {
   creditCard: ICreditCard;
-  handleDeleteCard: (id: string) => void;
+  handleDeleteCard?: (id: string) => void;
 }
 
 export default function CreditCard({ creditCard, handleDeleteCard }: ICreditCardProps) {
   return (
     <div
       key={creditCard.id}
-      className={`relative mt-2 rounded-3xl rounded-tr-[4.2rem] w-full h-52 p-2 ${creditCard.flag === 'mastercard' && 'bg-red-900'} ${creditCard.flag === 'visa' && 'bg-blue-800'} ${creditCard.flag == 'elo' && 'bg-black'}`}>
-      <svg
-        viewBox="0 0 100 100"
-        className="absolute top-0 right-0 w-12 h-12"
-        style={{ transform: 'rotate(-90deg)' }}>
-        <path className="dark:hidden" d="M 100 0 Q 0 0 0 100 L 100 100 Z" fill="#F3F4F6" />
+      className={`${handleDeleteCard ? 'relative my-2 rounded-tr-[4.2rem]' : 'flex'} rounded-3xl w-full h-52 p-2 ${creditCard.flag === 'mastercard' && 'bg-red-900'} ${creditCard.flag === 'visa' && 'bg-blue-800'} ${creditCard.flag == 'elo' && 'bg-black'}`}>
+      {handleDeleteCard && (
+        <>
+          <svg
+            viewBox="0 0 100 100"
+            className="absolute top-0 right-0 w-12 h-12"
+            style={{ transform: 'rotate(-90deg)' }}>
+            <path className="dark:hidden" d="M 100 0 Q 0 0 0 100 L 100 100 Z" fill="#F3F4F6" />
 
-        <path className="dark:flex hidden" d="M 100 0 Q 0 0 0 100 L 100 100 Z" fill="#27272A" />
-      </svg>
-      <Button
-        onClick={() => handleDeleteCard(creditCard.id)}
-        className="absolute top-1 right-1 text-red-600 p-0">
-        <DeleteIcon fontSize="large" color="inherit" />
-      </Button>
+            <path className="dark:flex hidden" d="M 100 0 Q 0 0 0 100 L 100 100 Z" fill="#27272A" />
+          </svg>
+          <Button
+            onClick={() => handleDeleteCard(creditCard.id)}
+            className="absolute top-1 right-1 text-red-600 p-0">
+            <DeleteIcon fontSize="large" color="inherit" />
+          </Button>
+        </>
+      )}
       <div className="flex flex-col w-full h-full items-stretch justify-between text-white font-bold text-3xl">
         <div className="flex flex-row items-center justify-between mt-2">
           {creditCard.flag === 'visa' && (
             <Image src={visaLogo} width={80} height={30} alt="Logo visa" />
           )}
+
           {creditCard.flag === 'mastercard' && (
             <Image src={mastercardLogo} width={80} height={30} alt="Logo mastercard" />
           )}
+
           {creditCard.flag === 'elo' && (
             <Image src={eloLogo} width={80} height={30} alt="Logo elo" />
           )}
-          <div className="mr-12 font-normal">**** {creditCard.cardNumber.slice(-4)}</div>
+
+          <div className={`${handleDeleteCard ? 'mr-12' : ''} font-normal`}>
+            **** {creditCard.cardNumber.slice(-4)}
+          </div>
         </div>
+        {!handleDeleteCard && (
+          <div className="flex flex-col items-center">
+            <span className="text-sm">Limite disponível</span>
+            <span className="text-4xl">R${creditCard.availableLimit}</span>
+          </div>
+        )}
+
         <div className="flex flex-row items-center justify-between p-1">
           <div>
             <div>{creditCard.nickname}</div>
+
             <div className="text-sm font-light">{creditCard.financialInstitution}</div>
           </div>
+
           <div className="font-normal text-2xl">
             {creditCard.expirationDate.getMonth() + 1}/
             {creditCard.expirationDate.getFullYear().toString().slice(-2)}
--- src/components/mobile/dashboardMobile/components/BalanceCard.tsx
+++ src/components/mobile/dashboardMobile/components/BalanceCard.tsx
@@ -1,11 +1,11 @@
 import { IAccount } from '@/types/account';
 import { ICreditCard } from '@/types/creditCard';
-import fetcher from '@/utils/fetcher';
 import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
 import CreditCardIcon from '@mui/icons-material/CreditCard';
 import HistoryIcon from '@mui/icons-material/History';
 import PaymentsIcon from '@mui/icons-material/Payments';
-import useSWR from 'swr';
+
+import CreditCard from '@/components/common/CreditCard';
 
 import QuickMenuButton from './QuickMenuButton';
 
@@ -28,40 +28,30 @@ export default function BalanceCard({
   selectedAccount,
   selectedCreditCard,
 }: BalanceCardProps) {
-  let account;
-  let creditCard;
+  const account = selectedAccount && accounts.find((acc) => acc.id === selectedAccount.id);
+  const creditCard =
+    selectedCreditCard && creditCards.find((cc) => cc.id === selectedCreditCard.id);
 
-  if (selectedCreditCard) {
-    if (creditCards) {
-      creditCard = creditCards.find((creditCard) => creditCard.id === selectedCreditCard.id);
-    }
-    if (!creditCard) {
-      return;
-    }
-  }
-
-  if (selectedAccount) {
-    if (accounts) {
-      account = accounts.find((account) => account.id === selectedAccount.id);
-    }
-    if (!account) {
-      return;
-    }
-  }
   return (
-    <div className="m-4 rounded-2xl dark:bg-[#000826] bg-[#121826] h-[13rem] text-white">
-      {creditCard && (
-        <div className="bg-green-500 rounded-2xl h-1/2 flex flex-col items-center justify-center"></div>
-      )}
-
-      {account && (
+    <div
+      className={`m-4 mt-5 rounded-3xl dark:bg-[#000826] bg-[#121826] ${creditCard ? 'h-[19rem]' : 'h-[13rem]'} text-white`}>
+      {creditCard ? (
+        <div className="rounded-3xl h-[70%] flex flex-col items-center justify-start">
+          <CreditCard creditCard={creditCard} />
+        </div>
+      ) : account ? (
         <div className="bg-green-500 rounded-2xl h-1/2 flex flex-col items-center justify-center">
           <h1 className="text-center text-lg">Seu balanço {account.nickname}</h1>
           <p className="text-center text-4xl font-bold">R${account.balance.toFixed(2)}</p>
         </div>
+      ) : (
+        <div className="h-1/2 flex items-center justify-center">
+          <p className="text-center text-lg">Nenhuma conta ou cartão selecionado</p>
+        </div>
       )}
 
-      <div className="dark:bg-[#000826] bg-[#121826] h-1/2 rounded-b-2xl flex flex-row justify-between">
+      <div
+        className={`dark:bg-[#000826] bg-[#121826] ${creditCard ? 'h-[30%]' : 'h-1/2'} rounded-b-2xl flex flex-row items-center justify-between`}>
         <QuickMenuButton label="Adicionar" onClick={openCreateItemModal}>
           <PaymentsIcon fontSize="large" />
         </QuickMenuButton>
