dbfcdc1b9bb7dc3ef1f874607d299a4e74569538
Heitor Lima Moreira <61924603+heitorlimamorei@users.noreply.github.com>
2024-11-22 16:20:24 -0300

    fix: removing unused use state import from useValidateRecurringItem

--- src/hook/useValidateRecurringItem.ts
+++ src/hook/useValidateRecurringItem.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 
 import { getDifferenceInHours, toggleJsonToDate } from '@/utils/datefunctions';
 import axios from 'axios';
